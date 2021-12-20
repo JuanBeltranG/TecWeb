ee3d1145416e859eb01bd49f895605a04d8ec24a
nero-ventus <devill_k.evin@hotmail.com>
2021-12-20 15:33:01 -0600

    n0m3l0

--- Proyecto/Controlador/ConexionBD.php
+++ Proyecto/Controlador/ConexionBD.php
@@ -5,7 +5,7 @@
         class Conexion {
         public $url = "localhost";
         public $user = "root";
-        public $psw = "dany1905";
+        public $psw = "n0m3l0";
         public $bd = "NuevoIngresoESCOM";
         public $port = 3306;
         public $mysqli;
